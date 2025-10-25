9bb92560fe530e66380e4205eb41f6a950043796
mraiko23 <aiearnideas@gmail.com>
2025-10-25 12:13:06 +0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  # Suppress source map warnings for node_modules packages
+  config.assets.quiet = true
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
