8c16210e37ad290d98338b6037f142dfb77eaf18
mraiko23 <mraiko23@gmail.com>
2025-10-25 02:42:13 -0400

    Fix Puma port binding conflict - CRITICAL FIX
    PROBLEM: Address already in use - bind(2) for 0.0.0.0 port 8080
    ROOT CAUSE: Duplicate bind and port directives in puma.rb
    SOLUTION: Remove bind directive, use only port directive
    
    This was causing infinite crash loop:
    - Puma tried to bind to 0.0.0.0:8080 twice
    - Failed with EADDRINUSE
    - Railway restarted it
    - Crashed again
    - REPEAT FOREVER
    
    Now uses only PORT environment variable from Railway.

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,10 @@
 # Puma configuration file
 
-# Set the number of worker threads (Railway recommends 5)
+# Set the number of worker threads
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000
+# Specifies the `port` that Puma will listen on to receive requests
 # Railway automatically sets the PORT environment variable
 port ENV.fetch("PORT", 3000)
 
@@ -12,8 +12,7 @@ port ENV.fetch("PORT", 3000)
 environment ENV.fetch("RAILS_ENV", "development")
 
 # Specifies the number of `workers` to boot in clustered mode
-# Workers are forked web server processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`
+# Workers are forked web server processes
 # Railway free tier works best with 0-2 workers
 workers ENV.fetch("WEB_CONCURRENCY", 2)
 
@@ -29,18 +28,9 @@ plugin :tmp_restart
 # Specify the PID file location
 pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid") if ENV["PIDFILE"]
 
-# Bind to 0.0.0.0 to accept connections from any IP (required for Railway)
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
-
-# Set the log file location
-stdout_redirect ENV.fetch("PUMA_STDOUT", "log/puma_stdout.log"), 
-                ENV.fetch("PUMA_STDERR", "log/puma_stderr.log"), 
-                true if ENV["PUMA_STDOUT"]
-
 # Configure worker lifecycle
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
